e1d46da7e839b0f518379bcbf008ae27b46f43c7
Jonatas <jonataseduardo@gmail.com>
2020-10-05 19:14:22 -0300

    rm install

--- install.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env sh
-
-cd $HOME/.config
-git clone git@github.com:jonataseduardo/tmux.git
-ln -s $HOME/.config/tmux.conf $HOME/.tmux.conf
-ln -s $HOME/.config/tmux.conf.local $HOME/.tmux.conf.local
